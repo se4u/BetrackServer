19010c4ad5b391f475429f769e6e5c31fbe2558f
cvincent <cvincent@witekio.com>
2017-10-15 19:30:28 -0700

    Add support for NOTIFICATION_RECEIVED

---
+++ android/BeTrackPostNotifRcv_SSL.php
@@ -0,0 +1,39 @@
+<?php
+
+include './BeTrackCrypto.php';
+
+if($result === false) {
+    goto endsession;
+}
+
+$date = '';
+$time = '';
+
+list($date, $time) = explode(chr (30), $plain);
+
+//Check the data
+$userid = strip_tags(trim($userid));
+$userid = mysqli_real_escape_string($con, $userid);
+
+$date = strip_tags(trim($date));
+$date = mysqli_real_escape_string($con, $date);
+
+$time = strip_tags(trim($time));
+$time = mysqli_real_escape_string($con, $time);
+
+$result = mysqli_query($con,"INSERT INTO BetrackNotificationRcv (UserId, Date, Time) 
+          VALUES ('$userid', '$date', '$time')");
+
+endsession:
+if($result === true) {
+    echo 'OK';
+}
+else{
+    echo("Error description: " . mysqli_error($con));
+	echo PHP_EOL;
+	echo 'KO';
+}
+
+mysqli_close($con);
+
+?>
